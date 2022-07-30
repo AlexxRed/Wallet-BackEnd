2ac94b12609562d689a3c06790a869b6368c38c0
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-07-30 14:09:49 +0300

    added transaction

---
+++ models/transaction.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const transactionSchema = new Schema(
+    {
+        amount: {
+            type: Number,
+            require: true,
+            set: (data) => Number(data),
+        },
+        type: {
+            type: String,
+            enum: ["+", "-"],
+            require: true,
+        },
+        category: {
+            type: String,
+            require: true,
+        },
+        date: {
+            type: String,
+            require: true,
+        },
+        month: {
+            type: Number,
+        },
+        year: {
+            type: Number,
+        },
+        balance: {
+            type: Number,
+        },
+        comment: {
+            type: String,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+        },
+    },
+    { versionKey: false, timestamps: true },
+)
+
+const Transaction = model("transaction", transactionSchema)
+
+const joiCreateTransaction = Joi.object({
+    amount: Joi.number().required(),
+    date: Joi.date().required(),
+    category: Joi.string().required(),
+    comment: [Joi.string(), Joi.number()],
+    type: Joi.string().required(),
+});
+
+module.exports = {
+    Transaction,
+    joiCreateTransaction,
+};
