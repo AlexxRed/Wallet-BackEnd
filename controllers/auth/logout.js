77630c82aef89f0da0f98a5c32918e092b09ec87
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-08-02 19:11:32 +0300

    fixed path

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,8 +1,9 @@
 const {User} = require("../../models/user");
 
 const logout = async(req, res)=> {
-    const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token: ""});
+    const { _id } = req.user;
+    const result = await User.findByIdAndUpdate(_id, { token: "" });
+    console.log(result);
     res.status(204).json();
 }
 
