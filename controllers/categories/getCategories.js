f9930e2fc9833d3891b67a11e12bb3c33489c2c6
Bohdan-Vozniak <bogdan2907@gmail.com>
2022-08-06 16:56:59 +0300

    fix: add balanse

--- controllers/categories/getCategories.js
+++ controllers/categories/getCategories.js
@@ -6,7 +6,7 @@ const categoriesPath = path.join(__dirname, "../../models/categories.json");
 const getCategories = async (req, res, next) => {
   const result = await fs.readFile(categoriesPath, "utf-8");
   const jsonResult = JSON.parse(result);
-  res.json({ ...jsonResult });
+  res.json([...jsonResult]);
 };
 
 module.exports = getCategories;
